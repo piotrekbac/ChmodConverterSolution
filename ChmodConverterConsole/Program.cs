a4f60642dd5999499f12efb1d0baadb5e8e3b50c
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-05-21 15:41:00 +0200

    commit 4

--- ChmodConverterConsole/Program.cs
+++ ChmodConverterConsole/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ChmodConverterLib;
+using System; 
+
+//Piotr Bacior 15 722 - WSEI Kraków
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Sprawdzamy czy użytkownik podał odpowiednie argumenty do programu
+        if (args.Length  != 1)
+        {
+            Console.WriteLine("Używamy trybu symbolicznego/numerycznego programu chmodconverter");
+            return;
+        }    
+
+        //Pobieramy teraz argument podany przez użytkownika
+        string input = args[0];
+
+        try
+        {
+            //Sprawdzamy czy wejście składa się z 9 znaków oraz czy są to znaki r, w, x, - odpowiadające za zapis symboliczny
+            if (input.Length == 9 && input.All(c => "rwx-".Contains(c)))
+            {
+                string numeric = ChmodConverter.SymbolicToNumeric(input);
+                Console.WriteLine(numeric);
+            }
+            //Sprawdzamy czy wejście składa się z 3 cyfr oraz czy są to cyfry (0-7) odpowiadające za zapis numeryczny
+            else if (input.Length == 3 && input.All(char.IsDigit))
+            {
+                string symbolic = ChmodConverter.NumericToSymbolic(input);
+                Console.WriteLine(symbolic);
+            }
+            else
+            {
+                Console.WriteLine("Niepoprawny format wejścia. Użyj trybu symbolicznego lub numerycznego.");
+            }
+        }
+        catch (Exception ex)
+        {
+            //W przypadku wystąpienia błędu, wyświetlamy komunikat o błędzie
+            Console.WriteLine($"Wystąpił błąd: {ex.Message}");
+        }
+    }
+}
